1dc877f2cd03d2f46ad75f8aeacc4bf89600ce4b
Muhamad Syihabudin <syehab94@gmail.com>
2026-01-20 23:31:01 +0700

    Add completed products CRUD functions

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"kasir-api/handler"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handler.GetProducts(w, r)
+		case http.MethodPost:
+			handler.CreateProduct(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.HandleFunc("/product", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handler.GetProduct(w, r)
+		case http.MethodPut:
+			handler.UpdateProduct(w, r)
+		case http.MethodDelete:
+			handler.DeleteProduct(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+	fmt.Println("Server running on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Failed to run server: %v", err)
+	}
+}

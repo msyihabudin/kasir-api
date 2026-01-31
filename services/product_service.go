38dae243a670e1d1e4e0f534e5fd5e08bbc268db
Muhamad Syihabudin <syehab94@gmail.com>
2026-01-31 18:42:30 +0700

    Add new product service

---
+++ services/product_service.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"kasir-api/models"
+	"kasir-api/repositories"
+)
+
+type ProductService struct {
+	repo *repositories.ProductRepository
+}
+
+func NewProductService(repo *repositories.ProductRepository) *ProductService {
+	return &ProductService{repo: repo}
+}
+
+func (s *ProductService) GetAll() ([]*models.Product, error) {
+	return s.repo.GetAll()
+}
+
+func (s *ProductService) Create(p *models.Product) (*models.Product, error) {
+	return s.repo.Create(p)
+}
+
+func (s *ProductService) GetByID(id string) (*models.Product, error) {
+	return s.repo.GetByID(id)
+}
+
+func (s *ProductService) Update(p *models.Product) (*models.Product, error) {
+	return s.repo.Update(p)
+}
+
+func (s *ProductService) Delete(id string) error {
+	return s.repo.Delete(id)
+}

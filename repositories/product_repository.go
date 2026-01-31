4d38cd94c9780049ec4a60a578ef63e67f5093a7
Muhamad Syihabudin <syehab94@gmail.com>
2026-01-31 19:30:19 +0700

    Adjust product repository

--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -1,22 +1,21 @@
 package repositories
 
 import (
-	"context"
+	"database/sql"
 	"kasir-api/models"
-
-	"github.com/jackc/pgx/v5"
+	// "github.com/jackc/pgx/v5"
 )
 
 type ProductRepository struct {
-	db *pgx.Conn
+	db *sql.DB
 }
 
-func NewProductRepository(db *pgx.Conn) *ProductRepository {
+func NewProductRepository(db *sql.DB) *ProductRepository {
 	return &ProductRepository{db: db}
 }
 
 func (r *ProductRepository) GetAll() ([]*models.Product, error) {
-	rows, err := r.db.Query(context.Background(), "SELECT id, name, price, stock FROM product")
+	rows, err := r.db.Query("SELECT id, name, price, stock FROM product")
 	if err != nil {
 		return nil, err
 	}
@@ -36,7 +35,6 @@ func (r *ProductRepository) GetAll() ([]*models.Product, error) {
 func (r *ProductRepository) Create(p *models.Product) (*models.Product, error) {
 	var id string
 	err := r.db.QueryRow(
-		context.Background(),
 		"INSERT INTO product (name, price, stock) VALUES ($1, $2, $3) RETURNING id",
 		p.Name, p.Price, p.Stock,
 	).Scan(&id)
@@ -50,7 +48,6 @@ func (r *ProductRepository) Create(p *models.Product) (*models.Product, error) {
 func (r *ProductRepository) GetByID(id string) (*models.Product, error) {
 	var p models.Product
 	err := r.db.QueryRow(
-		context.Background(),
 		"SELECT id, name, price, stock FROM product WHERE id = $1",
 		id).Scan(&p.ID, &p.Name, &p.Price, &p.Stock)
 	if err != nil {
@@ -61,7 +58,6 @@ func (r *ProductRepository) GetByID(id string) (*models.Product, error) {
 
 func (r *ProductRepository) Update(p *models.Product) (*models.Product, error) {
 	_, err := r.db.Exec(
-		context.Background(),
 		"UPDATE product SET name = $1, price = $2, stock = $3 WHERE id = $4",
 		p.Name, p.Price, p.Stock, p.ID)
 	if err != nil {
@@ -72,7 +68,6 @@ func (r *ProductRepository) Update(p *models.Product) (*models.Product, error) {
 
 func (r *ProductRepository) Delete(id string) error {
 	_, err := r.db.Exec(
-		context.Background(),
 		"DELETE FROM product WHERE id = $1", id)
 	return err
 }
